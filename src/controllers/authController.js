f429fb301b4e15f8d098d8201439c049d6d725d8
juliano340 <juliano340@gmail.com>
2023-11-15 17:04:07 -0300

    Hashes para senhas

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,32 +1,85 @@
 const { createConnection } = require('../db');
+const bcrypt = require('bcrypt');
 
-// Função para autenticar o usuário
-const authenticateUser = (req, res) => {
-  const { email, senha } = req.body;
+// Função para registrar um novo usuário
+const registerUser = async (req, res) => {
+    const { email, senha } = req.body;
 
-  // Criar uma nova conexão com o banco de dados usando a função do arquivo db.js
-  const connection = createConnection();
+    try {
 
-  // Consultar o banco de dados para autenticação
-  connection.query(
-    'SELECT * FROM usuarios WHERE email = ? AND senha = ?',
-    [email, senha],
-    (error, results, fields) => {
-      // Fechar a conexão com o banco de dados
-      connection.end();
+        // Hash da senha
+        const hashedPassword = await bcrypt.hash(senha, 10);
 
-      if (error) {
-        console.error('Erro na consulta SQL:', error);
+
+        // Criar uma nova conexão com o banco de dados usando a função do arquivo db.js
+        const connection = createConnection();
+
+        // Inserir usuário com senha hash no banco de dados
+        connection.query(
+            'INSERT INTO usuarios (email, senha) VALUES (?, ?)',
+            [email, hashedPassword],
+            (error, results, fields) => {
+                // Fechar a conexão com o banco de dados
+                connection.end();
+
+                if (error) {
+                    console.error('Erro na consulta SQL:', error);
+                    return res.status(500).json({ error: 'Erro no servidor' });
+                }
+
+                return res.json({ message: 'Usuário registrado com sucesso!' });
+            }
+        );
+    } catch (error) {
+        console.error('Erro ao criar hash de senha:', error);
         return res.status(500).json({ error: 'Erro no servidor' });
-      }
-
-      if (results.length > 0) {
-        return res.json({ message: 'Usuário autenticado com sucesso!' });
-      } else {
-        return res.status(401).json({ error: 'Credenciais inválidas' });
-      }
     }
-  );
 };
 
-module.exports = { authenticateUser };
+// Função para autenticar o usuário
+const authenticateUser = async (req, res) => {
+    const { email, senha } = req.body;
+
+
+    // Criar uma nova conexão com o banco de dados usando a função do arquivo db.js
+    const connection = createConnection();
+
+    // Consultar o banco de dados para obter a senha hash
+    connection.query(
+        'SELECT * FROM usuarios WHERE email = ?',
+        [email],
+        async (error, results, fields) => {
+            // Fechar a conexão com o banco de dados
+            connection.end();
+
+            if (error) {
+                console.error('Erro na consulta SQL:', error);
+                return res.status(500).json({ error: 'Erro no servidor' });
+            }
+
+            if (results.length === 0) {
+                return res.status(401).json({ error: 'Credenciais inválidas' });
+            }
+
+            const storedHashedPassword = results[0].senha;
+            const hashedPassword = await bcrypt.hash(storedHashedPassword, 10);
+            console.log(hashedPassword);
+
+            try {
+                // Comparar a senha fornecida com o hash armazenado no banco de dados
+                const match = await bcrypt.compare(senha, storedHashedPassword);
+
+                if (match) {
+                    return res.json({ message: 'Usuário autenticado com sucesso!' });
+                } else {
+                    return res.status(401).json({ error: 'Credenciais inválidas' });
+                }
+            } catch (error) {
+                console.error('Erro ao comparar hashes de senha:', error);
+                return res.status(500).json({ error: 'Erro no servidor' });
+            }
+        }
+    );
+};
+
+module.exports = { registerUser, authenticateUser };
